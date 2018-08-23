40e90d0533ac61cb95b2d21ec13e0426dd6211c5
Vijaya Donthireddy <donmac@vijayas-mbp.lan>
2018-08-22 22:18:53 -0700

    working copy

--- vjservice/hello.groovy
+++ vjservice/hello.groovy
@@ -4,6 +4,6 @@
 class HelloWorldController {
 	@RequestMapping("/names")
 	String hello() {
-		"Thanks for requesting a response from vjService!!!\n"
+		"Thanks for requesting a response from vjService - 001   !!!\n"
 	}
 }
