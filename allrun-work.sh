1cec1e0a4b2cb54ab1674d80c6a358fe1a568819
vdonthireddy <vijaya.donthireddy@ge.com>
2018-08-23 12:08:49 -0700

    updated bash file

--- allrun-work.sh
+++ allrun-work.sh
@@ -3,9 +3,8 @@ docker build -t vjapp .
 docker tag vjapp dtr.predix.io/212368581/vjapp:1.0
 docker push dtr.predix.io/212368581/vjapp:1.0
 cd ~/work/multi-container/docker-mutli-container/vjnodeservice
-#spring jar hello.jar hello.groovy
 docker build -t vjnodeservice .
 docker tag vjnodeservice dtr.predix.io/212368581/vjnodeservice:1.0
 docker push dtr.predix.io/212368581/vjnodeservice:1.0
 cd ~/work/multi-container/docker-mutli-container
-#docker-compose up
+docker-compose up
